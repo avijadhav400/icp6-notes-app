763235dc739b78dc28b71848438d657115b03bb8
avijadhav400 <avijadhav400@gmail.com>
2024-03-15 22:35:35 +0530

    home route added

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./views/Home";
+import NewNote from "./views/NewNote";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/new",
+    element: <NewNote />,
+  },
+]);
+root.render(<RouterProvider router={(router)}/>);
---
+++ client/src/views/NewNote.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './NewNote.css'
+
+const NewNote = () => {
+  return (
+    <div>
+      <h1>NewNote</h1>
+    </div>
+  )
+}
+
+export default NewNote
